bca253d0f79321c7b56372890372fb1b3ceacb4a
Gome510 <bluehawkfire55@gmail.com>
2023-08-15 14:47:00 -0700

    Messages imports message-data
    The new Messages component imports data from message-data.json to create a list of Message components.

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Messages from "./Messages";
+
+export default function MainContent(){
+    return(
+        <div className="maincontent">
+            <Messages />
+        </div>
+    );
+}
