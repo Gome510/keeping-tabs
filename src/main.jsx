c5f74209abf6b9cc3202d2ef7917e7d215c950ce
Fabianv9 <fvillasenor@alphaworks.tech>
2023-09-07 13:29:05 -0700

    add root route

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<App />,
+    element: <App />,
+    children: [
+      {
+        path: "messages/:channelId",
+        // element: < />,
+      },
+    ],
   },
 ]);
 
