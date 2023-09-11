106fc696528fa6bdeaf7de2f503329aa3bee33ad
LiamS-H <liamsh@gmail.com>
2023-09-11 14:30:01 -0700

    fixed typo

--- src/Routes/home/index.jsx
+++ src/Routes/home/index.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     return (
         <div>
         <h1>Welcome!</h1>
-        <p>Use the sidebar on the right to navigate between channels.</p>
+        <p>Use the sidebar on the left to navigate between channels.</p>
         <p>You can read messages anonymously but you must sign in to post.</p>
         </div>
     )
